4e99ef3e0001cf0f8fedca8d18f7595a45ad2868
ejsr <ejsr08@gmail.com>
2018-11-01 12:20:15 -0400

    Cambios en la logica de registro

--- eia/urls.py
+++ eia/urls.py
@@ -16,12 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from users import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
 
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
-    path('', views.HomePageView.as_view(), name='home'),
+    #path('', views.HomePageView.as_view(), name='home'),
+    #path('', views.LoginView.as_view(), name='login'),
+    path('', auth_views.LoginView.as_view(), name='login')
 
 ]
