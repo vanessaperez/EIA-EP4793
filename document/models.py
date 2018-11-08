a3fc965119cf37e592b0398e0d97c17a8067784c
Vanessa <vane.arc@gmail.com>
2018-11-08 00:16:39 -0400

    Se creó la app document, se modificó el settings.py, urls y base.html

---
+++ document/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django import forms
+
+
+RAZON_CHOICES = [
+   ('pnatural', 'Natural'),
+   ('pjuridica', 'Juridica'),
+]
+
+class DatosProyecto(models.Model):
+    titulo = models.CharField(max_length=130)
+    ubicacion = models.CharField(max_length=130)
+    area = models.CharField(max_length=130)
+    tipo = models.CharField(max_length=130)
+
+class DatosSolicitante(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    cedula = models.PositiveIntegerField()
+    telefono = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+
+
+
+class DatosOrganizacion(models.Model):
+
+    nombre = models.CharField(max_length=60)
+    razon_social = forms.ChoiceField(choices=RAZON_CHOICES, widget=forms.RadioSelect())
+    rif = models.CharField(max_length=30)
+    dir_oficina = models.CharField(max_length=130)
+    representante_legal = models.CharField(max_length=130)
+    telefono = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+
---
+++ document/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('datos/', views.datos_proyecto, name='datos_proyecto'),
+    path('solicitante/', views.datos_solicitante, name='datos_solicitante'),
+    path('organizacion/', views.datos_organizacion, name='datos_organizacion'),
+]
