968bb89a88dbdc9a834e190c0ed2ab642bafc043
ejsr <ejsr08@gmail.com>
2018-11-29 12:04:33 -0400

    Cambios a vista de ver documentos

--- document/urls.py
+++ document/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from document.views import *
 
 urlpatterns = [
     path('datos/', views.datos_proyecto, name='datos_proyecto'),
    # path('solicitante/', views.datos_solicitante, name='datos_solicitante'),
     #path('organizacion/', views.datos_organizacion, name='datos_organizacion'),
-    path('documentos_intencion/', views.documentos_intencion, name='documentos_intencion'),
+    path('documentos_intencion/', DocumentosIntencion.as_view(), name='documentos_intencion'),
 ]
